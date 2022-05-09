466f53fa2217f37902a27bb8c3c0357d640ab235
59114_Chetana <deslechetna1999@gmail.com>
2022-05-09 23:30:43 +0530

    [Chetana] Added : Validate Email Id

--- TestUserRegestration/UnitTest1.cs
+++ TestUserRegestration/UnitTest1.cs
@@ -18,5 +18,12 @@ namespace TestUserRegestration
             bool lastName = us.CheckFirstName("Desale");
             Assert.IsTrue(lastName);
         }
+        [Test]
+        public void TestEmail()
+        {
+            User us = new User();
+            bool email = us.CheckEmail("Chetana+desale1999@gmail.com.in");
+            Assert.IsTrue(email);
+        }
     }
 }
--- UserRegistrationProblem/User.cs
+++ UserRegistrationProblem/User.cs
@@ -5,6 +5,7 @@ namespace UserRegistrationProblem
     {
         public static string First_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string EMAIL_REGEX = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
         public bool CheckFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, First_NAME_REGEX);
@@ -12,6 +13,10 @@ namespace UserRegistrationProblem
         public bool CheckLASTName(string lastName)
         {
             return Regex.IsMatch(lastName, LAST_NAME_REGEX);
+        }
+        public bool CheckEmail(string email)
+        {
+            return Regex.IsMatch(email, EMAIL_REGEX);
         }
     }
 }
