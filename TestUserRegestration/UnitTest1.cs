32a09b182b561703897f6d7ed28f49fbd8e06666
59114_Chetana <deslechetna1999@gmail.com>
2022-05-10 22:06:48 +0530

    [Chetana] Refacter :the program

--- TestUserRegestration/UnitTest1.cs
+++ TestUserRegestration/UnitTest1.cs
@@ -5,35 +5,35 @@ namespace TestUserRegestration
     public class UnitTest1
     {
         [Test]
-        public void TestFirstName()
+        public void GivenStringInput_WhenTestFirstName_ShouldReturnFirstName()
         {
             User us = new User();
             bool firstName = us.CheckFirstName("Chetana");
             Assert.IsTrue(firstName);
         }
         [Test]
-        public void TestLastName()
+        public void GivenStringInput_WhenTestLastName_ShouldReturnLastName()
         {
             User us = new User();
             bool lastName = us.CheckFirstName("Desale");
             Assert.IsTrue(lastName);
         }
         [Test]
-        public void TestEmail()
+        public void GivenStringInput_WhenTestEmail_ShouldReturnEmail()
         {
             User us = new User();
             bool email = us.CheckEmail("Chetana+desale1999@gmail.com.in");
             Assert.IsTrue(email);
         }
         [Test]
-        public void TestMobileNo()
+        public void GivenStringInput_WhenTestMobileNo_ShouldReturnMobileNo()
         {
             User us = new User();
             bool mobileNo = us.CheckMobNo("7987612345");
             Assert.IsTrue(mobileNo);
         }
         [Test]
-        public void TestPassword()
+        public void GivenStringInput_WhenTestPassword_ShouldReturnPassword()
         {
             User us = new User();
             bool password = us.CheckPassword("@123Xyzlmnopq");
--- UserRegistrationProblem/User.cs
+++ UserRegistrationProblem/User.cs
@@ -3,19 +3,17 @@ namespace UserRegistrationProblem
 {
     public class User
     {
-        public static string First_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
-        public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
         public static string MOBILE_NUMBER_REGEX = "^[6-9]{1}[0-9]{9}$";
         public static string PASSWORD_REGEX = "^[@*_$%&]{1}[0-9A-Za-z]{8,}$";
-
         public static string EMAIL_REGEX = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
         public bool CheckFirstName(string firstName)
         {
-            return Regex.IsMatch(firstName, First_NAME_REGEX);
+            return Regex.IsMatch(firstName, NAME_REGEX);
         }
         public bool CheckLASTName(string lastName)
         {
-            return Regex.IsMatch(lastName, LAST_NAME_REGEX);
+            return Regex.IsMatch(lastName, NAME_REGEX);
         }
         public bool CheckEmail(string email)
         {
