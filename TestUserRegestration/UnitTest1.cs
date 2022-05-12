d0d66c42f41117ba68c6f71a4ac120ba18aaeced
59114_Chetana <deslechetna1999@gmail.com>
2022-05-12 18:18:27 +0530

    [Chetana] added : Sample Emails To Test

--- TestUserRegestration/UnitTest1.cs
+++ TestUserRegestration/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace TestUserRegestration
         public void GivenStringInput_WhenTestUserRegistration_ShouldReturnLastName()
         {
             User us = new User();
-            bool lastName = us.CheckFirstName("Desale");
+            bool lastName = us.CheckLASTName("Desale");
             Assert.IsTrue(lastName);
         }
         [Test]
@@ -23,7 +23,25 @@ namespace TestUserRegestration
         {
             User us = new User();
             bool email = us.CheckEmail("Chetana+desale1999@gmail.com.in");
+            bool email1 = us.CheckEmail("abc@yahoo.com");
+            bool email2 = us.CheckEmail("abc-100@yahoo.com");
+            bool email3 = us.CheckEmail("abc.100@yahoo.com");
+            bool email4 = us.CheckEmail("abc111@abc.com");
+            bool email5 = us.CheckEmail("abc-100@abc.net");
+            bool email6 = us.CheckEmail("abc.100@abc.com.au");
+            bool email7 = us.CheckEmail("abc@1.com");
+            bool email8 = us.CheckEmail("abc@gmail.com.com");
+            bool email9 = us.CheckEmail("abc+100@gmail.com");
             Assert.IsTrue(email);
+            Assert.IsTrue(email1);
+            Assert.IsTrue(email2);
+            Assert.IsTrue(email3);
+            Assert.IsTrue(email4);
+            Assert.IsTrue(email5);
+            Assert.IsTrue(email6);
+            Assert.IsTrue(email7);
+            Assert.IsTrue(email8);
+            Assert.IsTrue(email9);
         }
         [Test]
         public void GivenStringInput_WhenTestUserRegistration_ShouldReturnMobileNo()
