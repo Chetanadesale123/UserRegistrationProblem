9d72e5786b0319502de0d9318fedfbcaba6965f4
59114_Chetana <deslechetna1999@gmail.com>
2022-05-23 23:19:34 +0530

    [Chetana] Added : Throw Custom Exception For Invalid User Details

--- TestUserRegestration/UnitTest1.cs
+++ TestUserRegestration/UnitTest1.cs
@@ -4,60 +4,94 @@ namespace TestUserRegestration
 {
     public class UnitTest1
     {
-        User us = new User();
         [Test]
-        public void GivenStringInput_WhenTestUserRegistration_ShouldReturnFirstName()
+        public void GivenStringInput_WhenTestUserRegistration_ShouldReturnFirstName(string message)
         {
-            User us = new User();
-            bool firstName = us.CheckFirstName("Chetana");
-            Assert.IsTrue(firstName);
+            try
+            {
+                User us = new User("INVALID_FIRSTNAME");
+                bool firstName = us.CheckFirstName("chetana");
+                Assert.IsTrue(firstName);
+            }
+            catch(UserDetailsException ex) 
+            {
+                Assert.AreEqual("FirstName is invalid", ex.Message);
+            }
+             
         }
         [Test]
-        public void GivenStringInput_WhenTestUserRegistration_ShouldReturnLastName()
+        public void GivenStringInput_WhenTestUserRegistration_ShouldReturnLastName(string message)
         {
-            User us = new User();
-            bool lastName = us.CheckLASTName("Desale");
-            Assert.IsTrue(lastName);
+            try
+            {
+                User us = new User("INVALID_LASTNAME");
+                bool lastName = us.CheckLASTName("Desale");
+                Assert.IsTrue(lastName);
+            }
+            catch(UserDetailsException ex)
+            {
+                Assert.AreEqual("LastName Is invalid", ex.Message);
+            }
         }
         [Test]
-        public void GivenStringInput_WhenTestUserRegistration_ShouldReturnEmail()
+        public void GivenStringInput_WhenTestUserRegistration_ShouldReturnEmail(string message)
         {
-            User us = new User();
-            
-            bool email = us.CheckEmail("Chetana+desale1999@gmail.com.in");
-            bool email1 = us.CheckEmail("abc@yahoo.com");
-            bool email2 = us.CheckEmail("abc-100@yahoo.com");
-            bool email3 = us.CheckEmail("abc.100@yahoo.com");
-            bool email4 = us.CheckEmail("abc111@abc.com");
-            bool email5 = us.CheckEmail("abc-100@abc.net");
-            bool email6 = us.CheckEmail("abc.100@abc.com.au");
-            bool email7 = us.CheckEmail("abc@1.com");
-            bool email8 = us.CheckEmail("abc@gmail.com.com");
-            bool email9 = us.CheckEmail("abc+100@gmail.com");
-            Assert.IsTrue(email);
-            Assert.IsTrue(email1);
-            Assert.IsTrue(email2);
-            Assert.IsTrue(email3);
-            Assert.IsTrue(email4);
-            Assert.IsTrue(email5);
-            Assert.IsTrue(email6);
-            Assert.IsTrue(email7);
-            Assert.IsTrue(email8);
-            Assert.IsTrue(email9);
+            try
+            {
+                User us = new User("INVALID_EMAIL");
+                bool email = us.CheckEmail("Chetana+desale1999@gmail.com.in");
+                bool email1 = us.CheckEmail("abc@yahoo.com");
+                bool email2 = us.CheckEmail("abc-100@yahoo.com");
+                bool email3 = us.CheckEmail("abc.100@yahoo.com");
+                bool email4 = us.CheckEmail("abc111@abc.com");
+                bool email5 = us.CheckEmail("abc-100@abc.net");
+                bool email6 = us.CheckEmail("abc.100@abc.com.au");
+                bool email7 = us.CheckEmail("abc@1.com");
+                bool email8 = us.CheckEmail("abc@gmail.com.com");
+                bool email9 = us.CheckEmail("abc+100@gmail.com");
+                Assert.IsTrue(email);
+                Assert.IsTrue(email1);
+                Assert.IsTrue(email2);
+                Assert.IsTrue(email3);
+                Assert.IsTrue(email4);
+                Assert.IsTrue(email5);
+                Assert.IsTrue(email6);
+                Assert.IsTrue(email7);
+                Assert.IsTrue(email8);
+                Assert.IsTrue(email9);
+            }
+            catch(UserDetailsException ex)
+            {
+                Assert.AreEqual("Email is Invalid",ex.Message);
+            }
         }
         [Test]
         public void GivenStringInput_WhenTestUserRegistration_ShouldReturnMobileNo()
         {
-            User us = new User();
-            bool mobileNo = us.CheckMobNo("7987612345");
-            Assert.IsTrue(mobileNo);
+            try
+            {
+                User us = new User("INVALID_MOBILENO"); 
+                bool mobileNo = us.CheckMobNo("7875278951");
+                Assert.IsTrue(mobileNo);
+            }
+            catch(UserDetailsException ex)
+            {
+                Assert.AreEqual("Mobile No is invalid", ex.Message);
+            }
         }
         [Test]
         public void GivenStringInput_WhenTestUserRegistration_ShouldReturnPassword()
         {
-            User us = new User();
-            bool password = us.CheckPassword("@India12");
-            Assert.IsTrue(password);
+            try
+            {
+                User us = new User("INVALID_PASSWORD");
+                bool password = us.CheckPassword("@India12");
+                Assert.IsTrue(password);
+            }
+            catch (UserDetailsException ex)
+            {
+                Assert.AreEqual("Password Is Invalid", ex.Message);
+            }
         }
         [Test]
         [TestCase("abc@yahoo.com")]
@@ -66,9 +100,9 @@ namespace TestUserRegestration
         [TestCase("abc.100@abc.com.au")]
         public void GivenStringInput_WhenTestUserRegistration_ShouldReturnEmailParameterizedTest(string emailAddress)
         {
-            User us = new User();
+            User us = new User("Chetana@.com");
             string result = us.CheckEmailsUsingParameterizedTest(emailAddress);
-            Assert.AreEqual(result,emailAddress);
+            Assert.AreEqual(result, emailAddress);
         }
     }
 }
--- UserRegistrationProblem/User.cs
+++ UserRegistrationProblem/User.cs
@@ -7,25 +7,86 @@ namespace UserRegistrationProblem
         public static string MOBILE_NUMBER_REGEX = "^[6-9]{1}[0-9]{9}$";
         public static string EMAIL_REGEX = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
         public static string PASSWORD_REGEX = "^[!@#]{1}[A-Za-z0-9]{7,}$";
-        public bool CheckFirstName(string firstName)
+        public string message;
+        public string firstName;
+        public User(string message)
         {
-            return Regex.IsMatch(firstName, NAME_REGEX);
+            this.message = message;
+            
+        }
+        public bool CheckFirstName(string firstName)
+        { 
+            try
+            {
+                if (NAME_REGEX != firstName )
+                {
+                    throw new UserDetailsException(UserDetailsException.ExceptionType.INVALID_FIRSTNAME, "name is invalid");
+                }
+                return Regex.IsMatch(firstName, NAME_REGEX);
+            }
+            catch(UserDetailsException ex)
+            {
+                return ex.Message == "FirstName is invalid";
+            }
         }
         public bool CheckLASTName(string lastName)
         {
-            return Regex.IsMatch(lastName, NAME_REGEX);
+            try
+            {
+                if (NAME_REGEX != lastName)
+                {
+                    throw new UserDetailsException(UserDetailsException.ExceptionType.INVALID_LASTNAME, "Lastname is invalid");
+                }
+                return Regex.IsMatch(lastName, NAME_REGEX);
+            }
+            catch(UserDetailsException ex)
+            {
+                return ex.Message == "Last Name Is Invalid";
+            }
         }
         public bool CheckEmail(string email)
         {
-            return Regex.IsMatch(email, EMAIL_REGEX);
+            try
+            {
+                if (NAME_REGEX != email)
+                {
+                    throw new UserDetailsException(UserDetailsException.ExceptionType.INVALID_EMAIL, "Email is invalid");
+                }
+                return Regex.IsMatch(email, EMAIL_REGEX);
+            }catch(UserDetailsException ex)
+            {
+                return ex.Message == "Email is Invalid";
+            }
+            
         }
         public bool CheckMobNo(string mobileno)
         {
-            return Regex.IsMatch(mobileno, MOBILE_NUMBER_REGEX);
+            try
+            {
+                if (NAME_REGEX != mobileno)
+                {
+                    throw new UserDetailsException(UserDetailsException.ExceptionType.INVALID_MOBILENO, "Mobile is invalid");
+                }
+                return Regex.IsMatch(mobileno, MOBILE_NUMBER_REGEX);
+            }catch (UserDetailsException ex)
+            {
+                return ex.Message == "Mobile No is invalid";
+            }
         }
         public bool CheckPassword(string password)
         {
-            return Regex.IsMatch(password, PASSWORD_REGEX);
+            try
+            {
+                if (NAME_REGEX != password)
+                {
+                    throw new UserDetailsException(UserDetailsException.ExceptionType.INVALID_PASSWORD, "Password is invalid");
+                }
+                return Regex.IsMatch(password, PASSWORD_REGEX);
+            }
+            catch(UserDetailsException ex)
+            {
+                return ex.Message == "Password Is Invalid";
+            }
         }
         public string CheckEmailsUsingParameterizedTest(string emailAddress)
         {
